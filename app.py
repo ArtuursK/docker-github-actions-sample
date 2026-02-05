395ef322ac9ef6daa1fd169e9dd1a564ceffbcbd
Arturs Kempelis <arturs.kempelis_1@rtu.lv>
2026-02-05 12:44:04 +0200

    github actions test #2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Pass the required route to the decorator.
 @app.route("/hello")
 def hello():
-    return "Hello, Welcome to GeeksForGeeks"
+    return "Hello IZV"
 
 @app.route("/")
 def index():
